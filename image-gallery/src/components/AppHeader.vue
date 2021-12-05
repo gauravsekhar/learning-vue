d814e50ff6a4327b650dcc4634989e8d404d7b96
Gaurav Sekhar <gauravsekhar98@gmail.com>
2021-12-05 15:50:35 -0700

    use semantic UI for styling and setup vuex

---
+++ image-gallery/src/components/AppHeader.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="ui secondary pointing menu">
+    <a href="/" class="item active">
+      Image Storage
+    </a>
+  </div>  
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+
+</style>>
---
+++ image-gallery/src/store/index.js
@@ -0,0 +1,10 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);  // connection between the Vue and Vuex libraries
+
+export default new Vuex.Store({
+  modules: {
+
+  }
+});
