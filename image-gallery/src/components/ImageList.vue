12a81c3dc0c67829d132317e4a79588a296bec4a
Gaurav Sekhar <gauravsekhar98@gmail.com>
2021-12-14 21:15:24 -0700

    complete image list and upload form components

--- image-gallery/src/components/ImageList.vue
+++ image-gallery/src/components/ImageList.vue
@@ -1,15 +1,36 @@
 <template>
   <div>
-    ImageList
+    <div v-if="isLoggedIn" class="image-container">
+      <img v-for="image in allImages" :key="image.id" :src="image.link">
+    </div>
+    <h2 v-else>Please log in to get started!</h2>
   </div>
 </template>
 
 <script>
+import { mapActions, mapGetters } from 'vuex';
 export default {
   name: 'ImageList',
+  methods: {
+    ...mapActions(['fetchImages']),
+  },
+  computed: {
+    ...mapGetters(['allImages', 'isLoggedIn']),
+  },
+  created() {
+    // This is a lifecycle method
+    this.fetchImages();
+  }
 }
 </script>
 
 <style scoped>
-
+.image-container {
+  column-count: 3;
+  column-gap: 0;
+}
+img {
+  max-width: 100%;
+  padding: 5px
+}
 </style>
--- image-gallery/src/store/index.js
+++ image-gallery/src/store/index.js
@@ -1,11 +1,13 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 import auth from './modules/auth';
+import images from './modules/images';
 
 Vue.use(Vuex);  // initial handshake between Vue and Vuex libraries
 
 export default new Vuex.Store({
   modules: {
     auth,
+    images,
   }
 });
